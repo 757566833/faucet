034ffa347e0556e5231f45e5edd6770bff443b97
赵宏健 <757566833@qq.com>
2023-10-07 18:00:24 +0800

    mui

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ThemeRegistry from "@/context/theme";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -16,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ThemeRegistry>
+        <body className={inter.className}>{children}</body>
+      </ThemeRegistry>
     </html>
   );
 }
